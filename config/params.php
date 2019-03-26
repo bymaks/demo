2b66f3aad36d9c79151f4a2da92c2defa6a8861a
bymaks <34max01@gmail.com>
2019-03-27 00:07:25 +0700

    first

--- config/params.php
+++ config/params.php
@@ -2,7 +2,8 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'categoryMinut'=>[1,],// минуты солярия категории
+    'categoryMinut'=>[1,],// минуты категории
     'defaultGoods'=>[3,4,5],// изначаально отображаемые товары
     'maxDiscount'=>5,//максимальная скидка
+    'deltaDiscount'=>1,
 ];
